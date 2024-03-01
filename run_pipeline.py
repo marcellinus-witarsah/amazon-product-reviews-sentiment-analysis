82ffe8fb690782d9f68ab572eb8b01ace955ad0b
Marcellinus Aditya Witarsah (Orang Tua Group) <aw.marcel@gmail.com>
2024-03-01 14:04:46 +0700

    create python script for running the pipeline

---
+++ run_pipeline.py
@@ -0,0 +1,6 @@
+from pipelines.training_pipeline import training_pipeline
+
+if __name__ == "__main__":
+    training_pipeline(
+        data_path="D:/Documents/GitHub/amazon-product-reviews-sentiment-analysis/data/interim/sampled-raw-data.csv"
+    )
