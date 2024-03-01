da3ea57017ee2567af9f51766f8de6186e29f82f
Marcellinus Aditya Witarsah (Orang Tua Group) <aw.marcel@gmail.com>
2024-03-01 17:08:01 +0700

    add step for data splitting

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -10,11 +10,13 @@ from steps.ingest_data import ingest_data
 from steps.clean_data import clean_data
 from steps.label_data import label_data
 from steps.preprocess_data import preprocess_data
+from steps.split_data import split_data
 
 
 @pipeline
 def training_pipeline(data_path: str) -> pd.DataFrame:
     df = ingest_data(data_path=data_path)
-    df = clean_data(df)
-    df = label_data(df)
-    df = preprocess_data(df)
+    df = clean_data(df=df)
+    df = label_data(df=df)
+    df = preprocess_data(df=df)
+    X_train, X_val, X_test, y_train, y_val, y_test = split_data(df, test_size=0.3)
