937e34e07ef34ea83e45531bd8ec46b37665f980
Marcellinus Aditya Witarsah <aw.marcel@gmail.com>
2024-03-01 22:39:33 +0700

    udpate path for dataset

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,5 +2,5 @@ from pipelines.training_pipeline import training_pipeline
 
 if __name__ == "__main__":
     training_pipeline(
-        data_path="D:/Documents/GitHub/amazon-product-reviews-sentiment-analysis/data/interim/sampled-raw-data.csv"
+        data_path="data/interim/sampled-raw-data.csv"
     )
