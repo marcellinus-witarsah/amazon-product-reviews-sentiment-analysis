029d1b4fda733095106c1cf5f01460a373e1f924
Marcellinus Aditya Witarsah <aw.marcel@gmail.com>
2024-03-01 22:38:38 +0700

    combine previous steps into one

--- steps/preprocess_data.py
+++ steps/preprocess_data.py
@@ -3,12 +3,41 @@ import os
 import sys
 from dotenv import find_dotenv, load_dotenv
 from zenml import step
+from typing import Tuple
+from typing_extensions import Annotated
 
 load_dotenv(find_dotenv())
 sys.path.append(os.getenv("PROJECT_FOLDER"))
+from src.data.data_preprocessing import DataPreprocessStrategy
+from src.data.data_preprocessing import DataSplitStrategy
 from src.data.data_preprocessing import DataPreprocessing
 
 
 @step
-def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
-    return DataPreprocessing(df).preprocess_data()
+def preprocess_data(data: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"], 
+    Annotated[pd.DataFrame, "X_val"], 
+    Annotated[pd.Series, "y_train"], 
+    Annotated[pd.Series, "y_val"]
+]:
+    """
+    Perform data preprocessing from data cleaning, data labeling, text preprocessing, and data splitting
+    
+    Args:
+        data (pd.DataFrame): data
+
+    Returns:
+        pd.DataFrame: X_train 
+        pd.DataFrame: X_val 
+        pd.Series: y_train 
+        pd.Series: y_val
+    """
+    preprocess_strategy = DataPreprocessStrategy()
+    data_preprocessing = DataPreprocessing(data=data, strategy=preprocess_strategy)
+    preprocessed_data = data_preprocessing.handle_data()
+    
+    preprocess_strategy = DataSplitStrategy()
+    data_preprocessing = DataPreprocessing(data=preprocessed_data, strategy=preprocess_strategy)
+    X_train, X_test, y_train, y_test = data_preprocessing.handle_data()
+    
+    return X_train, X_test, y_train, y_test
