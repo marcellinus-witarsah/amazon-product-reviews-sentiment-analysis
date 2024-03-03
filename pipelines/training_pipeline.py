8a3db058a31b5a6e1a0156447bea92050e050105
Marcellinus Aditya Witarsah <aw.marcel@gmail.com>
2024-03-03 21:47:15 +0700

    add steps to training pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -7,14 +7,19 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 sys.path.append(os.getenv("PROJECT_FOLDER"))
 from steps.ingest_data import ingest_data
-from steps.clean_data import clean_data
-from steps.label_data import label_data
 from steps.preprocess_data import preprocess_data
-from steps.split_data import split_data
+from steps.train_model import train_model
+from steps.evaluate import evaluate
 
 
 @pipeline
-def training_pipeline(data_path: str) -> pd.DataFrame:
+def training_pipeline(data_path: str):
     data = ingest_data(data_path=data_path)
-    X_train, X_val, y_train, y_val = preprocess_data(data)
-    
+    X_train, X_test, y_train, y_test = preprocess_data(data)
+    model = train_model(X_train, y_train, **{})
+    accuracy_score, precision_score, recall_score, f1_score = evaluate(
+        model, X_test, y_test
+    )
+    print(
+        f"""Accuracy: {accuracy_score}, Precision: {precision_score}, Recall: {recall_score}, F1: {f1_score}"""
+    )
